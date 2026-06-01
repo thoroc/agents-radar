251138e6d6818788ec5852b44eb276cb413ce3f8
thoroc <thomas.a.roche@gmail.com>
2026-06-01 15:53:43 +0100

    test: add social prompt builder tests and scheduler config tests
    - Extract 3 social prompt builders to individual files (Rule 2)
    - Add tests for buildXiaohongshuPrompt, buildWechatPrompt, buildWechatMonthlyPrompt
    - Add scheduler config unit test
    - Current: 367 tests, 0 failing

--- src/notifications/social/index.ts
+++ src/notifications/social/index.ts
@@ -1,3 +1,6 @@
 export { socialCommand } from "./command";
 export { socialAction } from "./action";
 export type { SocialActionArgs, SocialDeps } from "./action";
+export { buildXiaohongshuPrompt } from "./build-xiaohongshu-prompt";
+export { buildWechatPrompt } from "./build-wechat-prompt";
+export { buildWechatMonthlyPrompt } from "./build-wechat-monthly-prompt";
